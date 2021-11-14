67cf6f640f7c2746641c0fb13718f9868b01c065
HariniU <64980694+HariniU@users.noreply.github.com>
2021-11-13 22:29:46 -0800

    JSDoc Setup

---
+++ source/jsdocTest.js
@@ -0,0 +1,16 @@
+//test file
+
+/**
+ * @type {string}
+ */
+ const word = 'Hello';
+
+ /**
+ * 
+ * @param {*} num1 first number
+ * @param {*} num2 second number
+ * @returns sum
+ */
+  function sum(num1, num2) {
+    return num1 + num2;  
+  }
