91b81ee125fd3519e3e4377df0f264cc534f4bed
Harini321 <62625417+Harini321@users.noreply.github.com>
2021-11-16 22:43:20 -0800

    Update jsdocTest.js

--- source/jsdocTest.js
+++ source/jsdocTest.js
@@ -5,11 +5,15 @@
  */
 // const word = 'Hello';
 
- /**
+/**
  * @param {*} num1 first number
  * @param {*} num2 second number
  * @returns sum
  */
 function sum(num1, num2) {
-  return num1 + num2;
+  return num1 + num2
+}
+
+function main() {
+  sum(3,4);
 }
