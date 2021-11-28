dbb79c0adeb28709010c57c688dd08669a2f8222
Madeline Yamamoto <madeline.yamamoto@gmail.com>
2021-11-28 11:26:21 -0800

    change backend_src folder name

--- testing/tests/backend.test.js
+++ testing/tests/backend.test.js
@@ -1,4 +1,4 @@
-const functions = require('../"backend src"/backend');
+const functions = require('../backend_src/backend');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(functions.sum2(1, 2)).toBe(3);
