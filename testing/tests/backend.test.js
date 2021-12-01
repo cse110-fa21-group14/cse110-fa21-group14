18e6de9e99139d95bd1c9d53775bd5ae571d9f09
Madeline Yamamoto <madeline.yamamoto@gmail.com>
2021-11-30 19:26:03 -0800

    backend unit testing fix

--- testing/tests/backend.test.js
+++ testing/tests/backend.test.js
@@ -2,5 +2,5 @@
 import { sum2, saveToLocalStorage, deleteRecipe, save, imgToURL, getAll, get } from '../backend src/backend.js';
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(functions.sum2(1,2)).toBe(3);
+    expect(sum2(1,2)).toBe(3);
   });
