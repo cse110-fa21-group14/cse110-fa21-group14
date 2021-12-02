dde397c678ceb8d4220a5de5609a92a86e2abf2f
THEODOREALOUCSD <tjalo@ucsd.edu>
2021-12-01 16:04:19 -0800

    Adding recommended recipe to my own list works

--- Recipe Code/assets/recommended.js
+++ Recipe Code/assets/recommended.js
@@ -1,3 +1,4 @@
+import {save} from '../../testing/backend src/backend.js';
 window.addEventListener('DOMContentLoaded', init);
 const search_bar = document.getElementById('search-bar');
 // Function to traverse recommended JSon object and
@@ -35,7 +36,39 @@ export function makeRecList(data) {
                 newInst.innerHTML = instList[i];
                 instructionList.appendChild(newInst);
             }
-           
+            //add to my own list
+            let addRecipe = document.getElementById('add-recipe');
+            if (addRecipe){
+                addRecipe.addEventListener('click', (event) =>{
+                    let date = Date.now();
+                    let newRecipe = {
+                        id: recipe.id,
+                        name: recipe.name,
+                        img: recipe.img,
+                        ingredients: {
+                            proportion: 1,
+                            ingredients: [],
+                        },
+                        steps: [],
+                        serving: 1,
+                        tags: [],
+                        made: new Date(date),
+                        created: new Date(date),
+                        makeCount: 0
+                    }
+                    for (let i = 0; i < ingList.length; i++) {
+                        newRecipe.ingredients['ingredients'].push({
+                            ingName: ingList[i]['name'],
+                            amount: ingList[i]['amount'],
+                            unit: ingList[i]['unit'],
+                        });
+                    }
+                    for (let i = 0; i < instList.length; i++) {
+                        newRecipe.steps.push(instList[i]);
+                    }
+                    save(newRecipe);
+                });
+            }
         });
        
         if(document.getElementById('recommended-list')){
