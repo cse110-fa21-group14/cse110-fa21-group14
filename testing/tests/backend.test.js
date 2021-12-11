f621cad6bdb393d3aab70fbddde29b29a4ebba3b
Makayla Nham <mdnham@ucsd.edu>
2021-12-10 18:08:09 -0800

    Changed source to test some functions

--- testing/tests/backend.test.js
+++ testing/tests/backend.test.js
@@ -1,4 +1,4 @@
-
+const { getRecipeData, getInstructions, getIngredients, getRecipeInfo } = require('../RecipeChamber/scripts/recommended.js');
 const API_KEY = "81fd56bfaac84f9c985e6c7474c02d60";
 //sample test
 test('adds 1 + 2 to equal 3', () => {
@@ -94,58 +94,58 @@ function sortAll(recipes, method) {
   }
 }
 
-/**
- * 
- * @param {string} recipe_name
- * @returns {Object} JSON Object
- * Call Spoonacular API for recipe basic information.
- * Information included:
- * id, title, image
- */
- async function getRecipeData(recipe_name) {
-    const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${recipe_name}&number=5`).then((response1) => {return response1});
-    return response.json();
-}
-/**
- * 
- * @param {string} recipe_id 
- * @returns {Object} JSON Object
- * Call Spoonacular API for recipe ingredients.
- * Information included:
- * name, amount, units
- */
-async function getIngredients(recipe_id) {
-    const response = await fetch(`https://api.spoonacular.com/recipes/${recipe_id}/ingredientWidget.json?apiKey=${API_KEY}`);
-    return response.json();
-}
+// /**
+//  * 
+//  * @param {string} recipe_name
+//  * @returns {Object} JSON Object
+//  * Call Spoonacular API for recipe basic information.
+//  * Information included:
+//  * id, title, image
+//  */
+//  async function getRecipeData(recipe_name) {
+//     const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${recipe_name}&number=5`).then((response1) => {return response1});
+//     return response.json();
+// }
+// /**
+//  * 
+//  * @param {string} recipe_id 
+//  * @returns {Object} JSON Object
+//  * Call Spoonacular API for recipe ingredients.
+//  * Information included:
+//  * name, amount, units
+//  */
+// async function getIngredients(recipe_id) {
+//     const response = await fetch(`https://api.spoonacular.com/recipes/${recipe_id}/ingredientWidget.json?apiKey=${API_KEY}`);
+//     return response.json();
+// }
 
-/**
- * 
- * @param {string} recipe_id 
- * @returns {Object} JSON Object
- * Call Spoonacular API for recipe instructions
- * Information included:
- * step, step number
- */
-async function getInstructions(recipe_id) {
-    const response = await fetch(`https://api.spoonacular.com/recipes/${recipe_id}/analyzedInstructions?apiKey=${API_KEY}`);
-    return response.json();
-}
+// /**
+//  * 
+//  * @param {string} recipe_id 
+//  * @returns {Object} JSON Object
+//  * Call Spoonacular API for recipe instructions
+//  * Information included:
+//  * step, step number
+//  */
+// async function getInstructions(recipe_id) {
+//     const response = await fetch(`https://api.spoonacular.com/recipes/${recipe_id}/analyzedInstructions?apiKey=${API_KEY}`);
+//     return response.json();
+// }
 
-async function getRecipeInfo(recipe_name) {
-    // Search for 10 recipes.
-    let recipe_call = await getRecipeData(recipe_name);
-    // Actual array of recipe JSON elements.
-    let recipes = recipe_call.results;
-    // List of search results to be populated.
-    let SEARCH_OBJ = {"search": []};
-    // Iterate and parse information.
-    if (recipes.length == 0) return;
-    for (let i = 0; i < recipes.length; i++) {
-       SEARCH_OBJ.search.push({"id": recipes[i].id, "name": recipes[i].title, "img": recipes[i].image, "ingList": [], instList: []});
-    }
-    return SEARCH_OBJ;
- }
+// async function getRecipeInfo(recipe_name) {
+//     // Search for 10 recipes.
+//     let recipe_call = await getRecipeData(recipe_name);
+//     // Actual array of recipe JSON elements.
+//     let recipes = recipe_call.results;
+//     // List of search results to be populated.
+//     let SEARCH_OBJ = {"search": []};
+//     // Iterate and parse information.
+//     if (recipes.length == 0) return;
+//     for (let i = 0; i < recipes.length; i++) {
+//        SEARCH_OBJ.search.push({"id": recipes[i].id, "name": recipes[i].title, "img": recipes[i].image, "ingList": [], instList: []});
+//     }
+//     return SEARCH_OBJ;
+//  }
 
 test('get recipe data test 1', () => {
     expect(getRecipeInfo('strawberry')).not.toBeNull();
